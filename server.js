1e83074135cd30f9c03f75425dfe94604a47c847
Luis H. Ball <luqven@gmail.com>
2020-10-22 10:58:05 -0400

    setup development and deployment env

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-
-// Importing Modules
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+
+// Constants
+const LOCAL_HOST = 'mongodb://localhost/my_database';
+const MONGOOSE_OPTIONS = { useNewUrlParser: true };
+const URL = process.env.MONGODB_URI || LOCAL_HOST
 
 // importing files
 const routes = require('./routes');
@@ -11,12 +14,36 @@ const routes = require('./routes');
 // Define Global Variables
 const app = express();
 const log = console.log;
-const PORT = process.env.PORT || 8080; // Step 1
+const PORT = process.env.PORT || 8080; // Default to port available in Heroku
 
 
-// Step 2
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
-    useNewUrlParser: true
+// Connect to MONGODB URI exposed by Heroku
+mongoose.connection.once('open', function () {
+    console.info('MongoDB event open');
+    console.debug('MongoDB connected [%s]', URL);
+
+    mongoose.connection.on('connected', function () {
+        console.info('MongoDB event connected');
+    });
+
+    mongoose.connection.on('disconnected', function () {
+        console.warn('MongoDB event disconnected');
+    });
+
+    mongoose.connection.on('reconnected', function () {
+        console.info('MongoDB event reconnected');
+    });
+
+    mongoose.connection.on('error', function (err) {
+        console.error('MongoDB event error: ' + err);
+    });
+});
+
+mongoose.connect(URL, function (err) {
+    if (err) {
+        console.error('MongoDB connection error: ' + err);
+        process.exit(1);
+    }
 });
 
 // Configuration
@@ -24,7 +51,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
 
-// Step 3
+// Send the static build files to the server
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
 
