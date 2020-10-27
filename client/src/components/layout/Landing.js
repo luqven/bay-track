3c0adcb6cb39292571e5595ccab387816c5a8e02
Luis H. Ball <luqven@gmail.com>
2020-10-27 10:10:31 -0400

    change landing text

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,13 +8,10 @@ class Landing extends Component {
         <div className="row">
           <div className="col s12 center-align">
             <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
+              <b>Track</b> Ebay price averages
             </h4>
             <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
+              Search for and track average price for Ebay items
             </p>
             <br />
             <div className="col s6">
