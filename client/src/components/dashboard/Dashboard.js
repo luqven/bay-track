22f262c1987edb76160a11dfef610c1d58d1555b
Luis H. Ball <luqven@gmail.com>
2020-11-02 16:25:58 -0500

    create price lookup form

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -22,8 +22,8 @@ class Dashboard extends Component {
             <h4>
               <b>Hey there,</b> {user.name.split(" ")[0]}
               <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+                You are logged into{" "}
+                <span style={{ fontFamily: "monospace" }}>BayPricer</span> app 👏
               </p>
             </h4>
             <button
--- client/src/components/priceLookup/LookupForm.js
+++ client/src/components/priceLookup/LookupForm.js
@@ -1,19 +1,74 @@
 import React from 'react';
+import { connect } from "react-redux";
 
-export default function StateTextFields() {
+import { fetchListing } from "../../actions/listingActions"
+
+
+const InputForm = function(props) {
     const [itemName, setName] = React.useState('');
+
     const handleChange = (event) => {
         setName(event.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(itemName)
+        props.fetchListing(itemName)
+        setName('')
+    }
+
+    const handleKeypress = e => {
+        if (e.keyCode === 13) {
+            handleSubmit();
+        }
     };
 
     console.log(itemName)
 
     return (
         <div class="input-field">
-            <form>
+            <form onSubmit={ e => handleSubmit(e)}>
                 <label htmlFor="first_name">Search</label>
-                <input onChange={handleChange} value={itemName} placeholder="item or listing name" id="first_name" type="text" class="validate"></input>
+                <input 
+                    onChange={handleChange} 
+                    onKeyPress={handleKeypress}
+                    value={itemName} 
+                    placeholder="item or listing name" 
+                    id="first_name" 
+                    type="text" 
+                    class="validate"
+                    >
+                </input>
             </form>
+            <ul>
+                {Object.keys(props.listings).map((id) => {
+                    return (
+                        <li>{
+                            '[' + 
+                            props.listings[id].price +
+                            '] ' +
+                            props.listings[id].title
+                        }</li>
+                        )
+                })}
+            </ul>
         </div>
     );
 }
+
+const mapStateToProps = (state) => {
+    debugger
+    return {
+        listings: {...state.listings}
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchListing: (name) => dispatch(fetchListing(name))
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(InputForm)
