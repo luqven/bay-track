9d08dbedcea3129a415eda14405360fe47a917ac
Luis H. Ball <luqven@gmail.com>
2020-11-06 11:13:29 -0500

    add logout button and change styling

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,27 +1,60 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { logoutUser } from "../../actions/authActions";
 
 class Navbar extends Component {
+
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
   render() {
     return (
-      <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper white">
-            <Link
-              to="/"
-              style={{
-                fontFamily: "monospace"
-              }}
-              className="col s5 brand-logo center black-text"
-            >
-              <i className="material-icons">code</i>
-              BayPricer
-            </Link>
-          </div>
-        </nav>
-      </div>
+      <nav>
+        <div className="nav-wrapper light-blue">
+          <ul id="nav-mobile" className="">
+            {this.props.auth.isAuthenticated === true &&
+              <li>
+                <button
+                  style={{
+                    width: 90,
+                    marginLeft: 25,
+                    borderRadius: "3px",
+                    letterSpacing: "1px",
+                  }}
+                  onClick={this.onLogoutClick}
+                  className="btn waves-effect waves-light black accent-3"
+                >
+                  Logout
+              </button>
+              </li>}
+          <Link
+            to="/"
+            className="col s5 brand-logo center"
+          >
+            BayPricer
+          </Link>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Navbar);
