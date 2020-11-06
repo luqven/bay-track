c866164ed952aaf5d17217b3ec674b5566ae0478
Luis H. Ball <luqven@gmail.com>
2020-11-06 11:13:44 -0500

    simplify dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,37 +7,13 @@ import { logoutUser } from "../../actions/authActions";
 import PriceLookup from "../priceLookup/priceLookupContainer";
 
 class Dashboard extends Component {
-  onLogoutClick = e => {
-    e.preventDefault();
-    this.props.logoutUser();
-  };
-
   render() {
     const { user } = this.props.auth;
 
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
+        <div className="row" style={{width: '100%'}}>
           <div className="landing-copy col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into{" "}
-                <span style={{ fontFamily: "monospace" }}>BayPricer</span> app 👏
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
             {/* Price lookup form */}
             <PriceLookup />
           </div>
