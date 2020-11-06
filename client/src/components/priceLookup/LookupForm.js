b7ed5db25bbde08e74652d7a413473a7279a08be
Luis H. Ball <luqven@gmail.com>
2020-11-06 11:14:20 -0500

    change styling and add button

--- client/src/components/priceLookup/LookupForm.js
+++ client/src/components/priceLookup/LookupForm.js
@@ -14,6 +14,11 @@ const InputForm = function(props) {
         setName(event.target.value);
     };
 
+    const clearName = (e) => {
+        e.preventDefault()
+        setName('')
+    }
+
     const handleKeypress = e => {
         if (e.keyCode === 13) {
             handleSubmit();
@@ -21,19 +26,25 @@ const InputForm = function(props) {
     };
 
     return (
-        <div className="input-field">
-            <form onSubmit={ e => handleSubmit(e, itemName)}>
-                <label htmlFor="first_name">Search</label>
-                <input 
-                    onChange={handleChange} 
+        <div className="nav-wrapper">
+            <form onSubmit={e => handleSubmit(e, itemName)}>
+                <div className="input-field z-depth-1" style={{height: 40}}>
+                    <input
+                    id="search" 
+                    type="search" 
+                    required
+                    onChange={handleChange}
                     onKeyPress={handleKeypress}
-                    value={itemName} 
-                    placeholder="item or listing name" 
-                    id="first_name" 
-                    type="text" 
-                    className="validate"
-                    >
-                </input>
+                    value={itemName}
+                    placeholder="item or listing name"
+                    id="listing_name" 
+                    />
+                        <label className="label-icon" htmlFor="search">
+                            <i style={{marginTop: 10}} className="material-icons">search</i>
+                        </label>
+                    <i style={{ marginTop: 5 }} onClick={clearName} className="material-icons"> close </i>
+                </div>
+                <button className="btn waves-effect waves-light blue accent-3">search</button>
             </form>
             <h4>{Object.keys(listings).length > 0 && averagePrice}</h4>
             <SerpResults listings={listings} />
