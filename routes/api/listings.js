19260ec488ef814c1380afada5d509002d136eb4
Luis H. Ball <luqven@gmail.com>
2020-11-16 13:35:05 -0500

    update truePrice package implementation

--- routes/api/listings.js
+++ routes/api/listings.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const {truePrice} = require('true-price');
+const { truePrice } = require("true-price");
 
-router.post('/search', (req, res) => {
-    console.log(req.body)
-    let listingName = req.body.listingName
-    let listing = truePrice.fetchAveragePrice(listingName).then(response => {
-        console.log('SERP for ' + listingName, response)
-        return res.json({
-            listings: response.listings,
-            averagePrice: '$' + Math.floor(response.averagePrice)
-        })
-    })
-})
+router.post("/search", (req, res) => {
+  console.log(req.body);
+  let listingName = req.body.listingName;
+  const options = {
+    type: "ebay",
+    verbose: true,
+  };
+  const ebay = truePrice.init(options);
+  let listing = ebay.scrapeFor(listingName).then((response) => {
+    console.log("SERP for " + listingName, response);
+    return res.json({
+      listings: response.listings,
+      averagePrice: "$" + Math.floor(response.averagePrice),
+    });
+  });
+});
 
 module.exports = router;
